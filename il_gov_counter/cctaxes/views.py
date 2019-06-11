c2747fcc3a594c59d6b88724f3933fe0fd931a4c
Sawyer Middeleer <saw.midd@gmail.com>
2019-06-11 12:04:28 -0500

    added some conditionals and charts

--- il_gov_counter/cctaxes/views.py
+++ il_gov_counter/cctaxes/views.py
@@ -35,9 +35,11 @@ def results(request, id):
     property_address.get_tax_code()
 
     all_codes = TaxCode.objects.filter(tax_year=2017).distinct().order_by('tax_code')
+    all_codes_14 = TaxCode.objects.filter(tax_year=2014).distinct()
 
-    prop_tax_code = TaxCode.objects.filter(tax_code=property_address.tax_code).order_by('-etr_share')
+    prop_tax_code = TaxCode.objects.filter(tax_code=property_address.tax_code).distinct().order_by('-etr_share')
     prop_tax_code_17 = prop_tax_code.filter(tax_year=2017)
+    prop_tax_code_14 = prop_tax_code.filter(tax_year=2014)
     prop_tax_17_df = read_frame(prop_tax_code_17)
     by_category = prop_tax_17_df.groupby('agency_type')['tax_rate_proportion'].sum()
 
@@ -77,29 +79,47 @@ def results(request, id):
     for code in all_codes:
         codes.add(code.tax_code)
 
-    etr = round(prop_tax_code_17[0].effective_property_tax_rate, 2)
+    etr = round(prop_tax_code_17[0].effective_property_tax_rate, 1)
+
     home_value = re.sub('[^0-9]','', property_address.value)
     payment = float(etr)/100*float(home_value)*.5
-    etr_summary = TaxCode.objects.filter(tax_year=2017).aggregate(Avg('effective_property_tax_rate'), Max('effective_property_tax_rate'), Min('effective_property_tax_rate'))
+
+    district_etr_summary = TaxCode.objects.filter(tax_year=2017, assessment_district=prop_tax_code[0].assessment_district).aggregate(Avg('effective_property_tax_rate'), Max('effective_property_tax_rate'), Min('effective_property_tax_rate'))
+
+    cook_county_avg_etr = all_codes.aggregate(Avg('effective_property_tax_rate'))['effective_property_tax_rate__avg']
+    cook_county_avg_etr_14 = all_codes_14.aggregate(Avg('effective_property_tax_rate'))['effective_property_tax_rate__avg']
+
+    tax_code_change = (prop_tax_code_17[0].tax_code_rate - prop_tax_code_14[0].tax_code_rate) / prop_tax_code_14[0].tax_code_rate * 100
+    county_change = (cook_county_avg_etr - cook_county_avg_etr_14) / cook_county_avg_etr_14 * 100
 
     context = {
                'prop_id':property_address.id,
                'prop_val':home_value,
                'prop_tax_code':property_address.tax_code,
-               'tax_code_rate':round(prop_tax_code_17[0].tax_code_rate,2),
+               'tax_code_rate':round(prop_tax_code_17[0].tax_code_rate,1),
+               'tax_code_rate_14':round(prop_tax_code_14[0].tax_code_rate,1),
                'body_count':prop_tax_code_17[0].taxing_body_count,
                'effective_property_tax_rate':etr,
                'payment':round(int(payment),-2),
                'township':prop_tax_code_17[0].assessment_district,
                'tax_data_table':tax_table,
                'category_data_table':category_table,
-               'township_etr':round(etr_summary['effective_property_tax_rate__avg'], 2),
-               'township_min':round(etr_summary['effective_property_tax_rate__min'], 2),
-               'township_max':round(etr_summary['effective_property_tax_rate__max'], 2),
+               'township_etr':round(district_etr_summary['effective_property_tax_rate__avg'], 1),
+               'township_min':round(district_etr_summary['effective_property_tax_rate__min'], 1),
+               'township_max':round(district_etr_summary['effective_property_tax_rate__max'], 1),
                'categories':categories,
                'category_etrs':category_etrs,
                'tax_codes':codes,
                'by_category':by_category,
+               'cook_county_avg_etr':round(cook_county_avg_etr,1),
+               'cook_county_avg_etr_14':round(cook_county_avg_etr_14,1),
+               'greater_township':prop_tax_code_17[0].effective_property_tax_rate > district_etr_summary['effective_property_tax_rate__avg'],
+               'lesser_township':prop_tax_code_17[0].effective_property_tax_rate < district_etr_summary['effective_property_tax_rate__avg'],
+               'greater_county':prop_tax_code_17[0].effective_property_tax_rate > all_codes.aggregate(Avg('effective_property_tax_rate'))['effective_property_tax_rate__avg'],
+               'lesser_county':prop_tax_code_17[0].effective_property_tax_rate < all_codes.aggregate(Avg('effective_property_tax_rate'))['effective_property_tax_rate__avg'],
+               'tax_code_change':round(tax_code_change,1),
+               'county_change':round(county_change,1),
+               
                'form':form,
                }
 
